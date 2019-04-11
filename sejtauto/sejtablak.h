74733c9169567077cd1163deb9e1dac349f7baf6
Schachinger Zsolt <35422187+salesz9902@users.noreply.github.com>
2019-04-11 08:26:34 +0200

    Rename sejtablak.h to sejtauto/sejtablak.h

--- sejtauto/sejtablak.h
+++ sejtauto/sejtablak.h
@@ -12,28 +12,29 @@ class SejtAblak : public QMainWindow
   Q_OBJECT
   
 public:
-  SejtAblak(int szelesseg = 100, int magassag = 75, QWidget *parent = 0);
+  SejtAblak(int szelesseg = 100, int magassag = 75, QWidget *parent = 0);
+
   ~SejtAblak();
-  // Egy sejt lehet Èlı
+  // Egy sejt lehet √©l≈ë
   static const bool ELO = true;
   // vagy halott
   static const bool HALOTT = false;
   void vissza(int racsIndex);
   
 protected:
-  // KÈt r·csot haszn·lunk majd, az egyik a sejttÈr ·llapot·t
-  // a t_n, a m·sik a t_n+1 idıpillanatban jellemzi.
+  // K√©t r√°csot haszn√°lunk majd, az egyik a sejtt√©r √°llapot√°t
+  // a t_n, a m√°sik a t_n+1 id≈ëpillanatban jellemzi.
   bool ***racsok;
-  // Valamelyik r·csra mutat, technikai jelleg˚, hogy ne kelljen a
-  // [2][][]-bÛl az elsı dimenziÛt haszn·lni, mert vagy az egyikre
-  // ·llÌtjuk, vagy a m·sikra.
+  // Valamelyik r√°csra mutat, technikai jelleg≈±, hogy ne kelljen a
+  // [2][][]-b√≥l az els≈ë dimenzi√≥t haszn√°lni, mert vagy az egyikre
+  // √°ll√≠tjuk, vagy a m√°sikra.
   bool **racs;
-  // Megmutatja melyik r·cs az aktu·lis: [r·csIndex][][]
+  // Megmutatja melyik r√°cs az aktu√°lis: [r√°csIndex][][]
   int racsIndex;
   // Pixelben egy cella adatai.
   int cellaSzelesseg;
   int cellaMagassag;
-  // A sejttÈr nagys·ga, azaz h·nyszor h·ny cella van?
+  // A sejtt√©r nagys√°ga, azaz h√°nyszor h√°ny cella van?
   int szelesseg;
   int magassag;    
   void paintEvent(QPaintEvent*);
