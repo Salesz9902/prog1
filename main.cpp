e3df427e797a3228ecfadd6b210ce00943e4e259
Schachinger Zsolt <35422187+salesz9902@users.noreply.github.com>
2019-04-11 08:25:31 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,25 +1,12 @@
-#include <iostream>
-#include "nlp.hpp"
-#include <png++/png.hpp>
+#include <QApplication>
+#include "sejtablak.h"
+#include <QDesktopWidget>
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-    png::image <png::rgb_pixel> png_image (argv[1]);
-
-    int size = png_image.get_width() * png_image.get_height();
-
-    Perceptron* p = new Perceptron(3, size, 256, 1);
-
-    double* image = new double[size];
-
-    for(int i{0}; i<png_image.get_width(); ++i)
-     for(int j{0}; j<png_image.get_height(); ++j)
-        image[i*png_image.get_width()+j] = png_image[i][j].red;
-
-    double value = (*p) (image);
-
-    std::cout << value << std::endl;
-
-    delete p;
-    delete [] image;
+  QApplication a(argc, argv);
+  SejtAblak w(100, 75);
+  w.show();
+  
+  return a.exec();
 }
